8ce0be69c23218971e0056ce9d13ec6380b963a3
1124096315 <1124096315@qq.com>
2021-01-10 17:07:23 +0800

    commit sushu.cpp

---
+++ sushu.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int n,d;
+	printf("Let us find the sushu <=n:");
+	scanf("%d",&n);
+	for(d=2;n>3;n--){
+		for(d=2;d<n;d++){
+		if(n%d==0&&d!==n){berak;
+		}else printf("%dÊÇËØÊý\n",n);break; }
+		 
+		
+		}
+	}
+}
